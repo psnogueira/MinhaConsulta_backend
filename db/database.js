793b037cb3b936d35b296772b9c19818c4f03496
Pedro Nogueira <psnogueira.e@gmail.com>
2024-10-16 10:20:05 -0300

    Aula 10/10/2024

--- db/database.js
+++ db/database.js
@@ -1,20 +1,30 @@
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('./consultas.db', (err) => {
-    if (err) {
-        console.error(err.message);
-    } else {
-        console.log('Conectado ao banco de dados SQLite.');
-    }
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log('Conectado ao banco de dados SQLite.');
+  }
 });
 
 // Criar a tabela de usuários
 db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS users (
+  db.run(`CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     role TEXT NOT NULL
+  )`);
+
+  // Criar a tabela de consultas
+  db.run(`CREATE TABLE IF NOT EXISTS consultations (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    doctor TEXT NOT NULL,
+    date TEXT NOT NULL,
+    status TEXT NOT NULL,
+    userId INTEGER,
+    FOREIGN KEY (userId) REFERENCES users(id)
   )`);
 });
 
